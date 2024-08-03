cc9efa91ad6af1eea13bae31228cff7efc46600e
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-08-03 11:28:57 -0400

    match `+`

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::env;
 use std::io;
-use std::iter::Peekable;
 use std::process;
 use std::str::Chars;
 
-#[derive(Debug)]
-enum Pattern {
-    Literal(char),
-    Digit,
-    Alphanumeric,
-    Group(bool, String),
+mod pattern;
+
+use pattern::{get_patterns, Pattern};
+
+fn match_regex(input_line: &str, mut regex: &str) -> bool {
+    let input_line = input_line.trim();
+    let start = if regex.starts_with('^') {
+        regex = &regex[1..];
+        true
+    } else {
+        false
+    };
+    let end = match regex.chars().last() {
+        Some(c) if c == '$' => {
+            regex = &regex[..regex.len() - 1];
+            true
+        }
+        _ => false,
+    };
+    let patterns = get_patterns(regex);
+    println!("{:?}", patterns);
+    match_substrings(input_line, &patterns, start, end)
 }
 
 fn match_substrings(
@@ -29,40 +44,71 @@ fn match_substrings(
     };
     let range_end = if start_anchor {
         // one iteration
-        1
+        range_start + 1
     } else {
         input_line.len()
     };
 
     'input_line: for i in range_start..range_end {
-        let mut substring = input_line[i..].chars();
-        for pattern in patterns {
+        let mut substring_iter = input_line[i..].chars();
+        let mut pattern_iter = patterns.iter();
+        while let Some(pattern) = pattern_iter.next() {
             match pattern {
                 Pattern::Literal(l) => {
-                    if !match_literal(&mut substring, *l) {
+                    if !match_literal(&mut substring_iter, *l) {
                         continue 'input_line;
                     }
                 }
                 Pattern::Digit => {
-                    if !match_digit(&mut substring) {
+                    if !match_digit(&mut substring_iter) {
                         continue 'input_line;
                     }
                 }
                 Pattern::Alphanumeric => {
-                    if !match_alphanumeric(&mut substring) {
+                    if !match_alphanumeric(&mut substring_iter) {
                         continue 'input_line;
                     }
                 }
                 Pattern::Group(positive, group) => {
-                    if match_group(&mut substring, group) != *positive {
+                    if match_group(&mut substring_iter, group) != *positive {
                         continue 'input_line;
                     }
+                }
+                Pattern::OneOrMore(pattern) => {
+                    let mut substring_copy = String::from_iter(substring_iter);
+                    let first_match = match_pattern(&substring_copy, pattern);
+                    if !first_match {
+                        continue 'input_line;
+                    }
+
+                    let remaining_patterns = pattern_iter.cloned().collect::<Vec<_>>();
+                    loop {
+                        substring_copy = substring_copy[1..].to_string();
+                        if match_substrings(&substring_copy, &remaining_patterns, false, false) {
+                            return true;
+                        }
+                        if !match_pattern(&substring_copy, pattern) {
+                            break;
+                        }
+                    }
+                    continue 'input_line;
                 }
             }
         }
         return true;
     }
     false
+}
+
+fn match_pattern(input_line: &str, pattern: &Pattern) -> bool {
+    let mut input_line = input_line.chars();
+    match pattern {
+        Pattern::Literal(l) => match_literal(&mut input_line, *l),
+        Pattern::Digit => match_digit(&mut input_line),
+        Pattern::Alphanumeric => match_alphanumeric(&mut input_line),
+        Pattern::Group(positive, group) => match_group(&mut input_line, group) == *positive,
+        p => panic!("Unexpected pattern: {:?}", p),
+    }
 }
 
 fn match_literal(chars: &mut Chars, literal: char) -> bool {
@@ -82,80 +128,6 @@ fn match_group(chars: &mut Chars, group: &str) -> bool {
     c.is_some_and(|c| group.contains(c))
 }
 
-fn match_pattern(input_line: &str, mut pattern: &str) -> bool {
-    let input_line = input_line.trim();
-    let start = if pattern.starts_with('^') {
-        pattern = &pattern[1..];
-        true
-    } else {
-        false
-    };
-    let end = match pattern.chars().last() {
-        Some(c) if c == '$' => {
-            pattern = &pattern[..pattern.len() - 1];
-            true
-        }
-        _ => false,
-    };
-    let patterns = get_patterns(pattern);
-    match_substrings(input_line, &patterns, start, end)
-}
-
-fn get_patterns(pattern: &str) -> Vec<Pattern> {
-    let mut patterns = Vec::new();
-    let mut chars = pattern.chars().peekable();
-
-    loop {
-        let c = chars.next();
-        if c.is_none() {
-            break;
-        }
-        let pattern = match c.unwrap() {
-            '\\' => {
-                let c = chars.next();
-                if c.is_none() {
-                    panic!("Expected character after '\\'");
-                }
-                match c.unwrap() {
-                    'd' => Pattern::Digit,
-                    'w' => Pattern::Alphanumeric,
-                    '\\' => Pattern::Literal('\\'),
-                    unknown => panic!("Unknown special character: {}", unknown),
-                }
-            }
-            '[' => {
-                let (is_positive, group) = get_group_pattern(&mut chars);
-                Pattern::Group(is_positive, group)
-            }
-            l => Pattern::Literal(l),
-        };
-        patterns.push(pattern);
-    }
-
-    patterns
-}
-
-fn get_group_pattern(chars: &mut Peekable<Chars>) -> (bool, String) {
-    let mut is_positive = true;
-    let mut group = String::new();
-
-    if chars.peek() == Some(&'^') {
-        is_positive = false;
-        chars.next();
-    }
-
-    while chars.peek() != Some(&']') {
-        let c = chars.next();
-        if c.is_none() {
-            panic!("Expected ']' after group");
-        }
-        group.push(c.unwrap());
-    }
-    chars.next();
-
-    (is_positive, group)
-}
-
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     if env::args().nth(1).unwrap() != "-E" {
@@ -168,7 +140,7 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    if match_pattern(&input_line, &pattern) {
+    if match_regex(&input_line, &pattern) {
         process::exit(0)
     } else {
         process::exit(1)
---
+++ src/pattern.rs
@@ -0,0 +1,70 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+#[derive(Debug, Clone)]
+pub enum Pattern {
+    Literal(char),
+    Digit,
+    Alphanumeric,
+    Group(bool, String),
+    OneOrMore(Box<Pattern>),
+}
+
+pub fn get_patterns(regex: &str) -> Vec<Pattern> {
+    let mut patterns = Vec::new();
+    let mut chars = regex.chars().peekable();
+
+    loop {
+        let c = chars.next();
+        if c.is_none() {
+            break;
+        }
+        let pattern = match c.unwrap() {
+            '\\' => {
+                let c = chars.next();
+                if c.is_none() {
+                    panic!("Expected character after '\\'");
+                }
+                match c.unwrap() {
+                    'd' => Pattern::Digit,
+                    'w' => Pattern::Alphanumeric,
+                    '\\' => Pattern::Literal('\\'),
+                    unknown => panic!("Unknown special character: {}", unknown),
+                }
+            }
+            '[' => {
+                let (is_positive, group) = get_group_pattern(&mut chars);
+                Pattern::Group(is_positive, group)
+            }
+            '+' => {
+                let pattern = patterns.pop().expect("Expected pattern before '+'");
+                Pattern::OneOrMore(Box::new(pattern))
+            }
+            l => Pattern::Literal(l),
+        };
+        patterns.push(pattern);
+    }
+
+    patterns
+}
+
+fn get_group_pattern(chars: &mut Peekable<Chars>) -> (bool, String) {
+    let mut is_positive = true;
+    let mut group = String::new();
+
+    if chars.peek() == Some(&'^') {
+        is_positive = false;
+        chars.next();
+    }
+
+    while chars.peek() != Some(&']') {
+        let c = chars.next();
+        if c.is_none() {
+            panic!("Expected ']' after group");
+        }
+        group.push(c.unwrap());
+    }
+    chars.next();
+
+    (is_positive, group)
+}
