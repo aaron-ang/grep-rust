f81889a1c73f5a90a007f11bb5e631da320dff66
Aaron Ang <aaron.ang@arm.com>
2025-08-11 17:05:21 -0700

    feat: search single line file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::{env, io, process};
+use std::{
+    env,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    process,
+};
 
 use anyhow::{anyhow, bail, Result};
 use colored::Colorize;
 
 use grep_starter_rust::match_regex;
 
-// Usage: echo <input_text> | your_program.sh -E <pattern>
+// Usage: echo <input_text> | your_program.sh -E <pattern> <file_name>
 fn main() -> Result<()> {
-    let mut args = env::args();
+    let mut args = env::args().skip(1);
 
-    let first_arg = args.nth(1);
-    if first_arg.is_none() || first_arg.unwrap() != "-E" {
+    let flag = args.next();
+    if flag.is_none() || flag.unwrap() != "-E" {
         bail!("Expected first argument to be '-E'");
     }
 
@@ -19,19 +24,27 @@ fn main() -> Result<()> {
         .ok_or_else(|| anyhow!("Expected second argument to be a pattern"))?;
 
     let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line)?;
-
-    if let Some(group) = match_regex(&input_line, &pattern) {
-        let i = input_line.find(&group).unwrap();
-        let j = i + group.len();
-        print!(
-            "{}{}{}",
-            input_line[..i].normal(),
-            input_line[i..j].bright_red().bold(),
-            input_line[j..].normal()
-        );
-        process::exit(0);
-    } else {
-        process::exit(1);
+    match args.next() {
+        Some(file_name) => {
+            let file = File::open(file_name)?;
+            BufReader::new(file).read_line(&mut input_line)?;
+        }
+        None => {
+            io::stdin().read_line(&mut input_line)?;
+        }
     }
+
+    if let Some(matched) = match_regex(&input_line, &pattern) {
+        if let Some(start) = input_line.find(&matched) {
+            let end = start + matched.len();
+            print!(
+                "{}{}{}",
+                input_line[..start].normal(),
+                input_line[start..end].bright_red().bold(),
+                input_line[end..].normal()
+            );
+            process::exit(0);
+        }
+    }
+    process::exit(1);
 }
