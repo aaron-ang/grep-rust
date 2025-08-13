a543686652f90456ab2d459773f7202e2faed05d
Aaron Ang <aaron.ang@arm.com>
2025-08-12 20:48:39 -0700

    feat: recursive search

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,109 @@
 use std::{
-    env,
     fs::File,
     io::{self, BufRead, BufReader},
+    path::{Path, PathBuf},
     process,
 };
 
-use anyhow::{anyhow, bail, Result};
+use anyhow::{bail, Result};
+use clap::{ArgAction, Parser};
 use colored::Colorize;
 
 use grep_starter_rust::match_regex;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Search for pattern in files
+    #[arg(short = 'E')]
+    pattern: String,
+
+    /// Recursively search through directories
+    #[arg(short, action = ArgAction::SetTrue)]
+    recursive: bool,
+
+    /// Files to search
+    #[arg(num_args = 0..)]
+    files: Vec<PathBuf>,
+}
+
 fn main() -> Result<()> {
-    let mut args = env::args().skip(1);
-
-    let flag = args.next();
-    if flag.is_none() || flag.unwrap() != "-E" {
-        bail!("Expected first argument to be '-E'");
-    }
-
-    let pattern = args
-        .next()
-        .ok_or_else(|| anyhow!("Expected second argument to be a pattern"))?;
-
-    fn process_lines<R: BufRead>(
-        reader: R,
-        pattern: &str,
-        filename_prefix: Option<&str>,
-    ) -> Result<usize> {
-        let mut match_count = 0;
-        let prefix = filename_prefix.map(|s| format!("{s}:")).unwrap_or_default();
-
-        for line in reader.lines() {
-            let line = line?;
-            if let Some(matched) = match_regex(&line, pattern) {
-                match_count += 1;
-
-                let output = match line.find(&matched) {
-                    Some(start) => {
-                        let end = start + matched.len();
-                        format!(
-                            "{}{}{}{}",
-                            prefix,
-                            &line[..start],
-                            &line[start..end].bright_red().bold(),
-                            &line[end..]
-                        )
-                    }
-                    None => format!("{prefix}{line}"),
-                };
-
-                println!("{output}");
-            }
-        }
-
-        Ok(match_count)
-    }
-
-    let files = args.collect::<Vec<_>>();
-    let match_count = if files.is_empty() {
+    let args = Args::parse();
+    let match_count = if args.files.is_empty() {
         let stdin = io::stdin();
         let reader = BufReader::new(stdin.lock());
-        process_lines(reader, &pattern, None)?
+        process_lines(reader, &args.pattern, None)?
     } else {
+        let file_paths = collect_files(&args.files, args.recursive)?;
         let mut total = 0;
-        let show_prefix = files.len() > 1;
-        for file_name in files {
-            let file = File::open(&file_name)?;
+        let show_prefix = file_paths.len() > 1;
+        for file_path in file_paths {
+            let file = File::open(&file_path)?;
             let reader = BufReader::new(file);
-            let prefix = show_prefix.then_some(file_name.as_str());
-            total += process_lines(reader, &pattern, prefix)?;
+            let prefix = show_prefix.then_some(file_path.to_str().unwrap());
+            total += process_lines(reader, &args.pattern, prefix)?;
         }
         total
     };
 
     process::exit(if match_count > 0 { 0 } else { 1 });
+}
+
+fn process_lines<R: BufRead>(reader: R, pattern: &str, filename: Option<&str>) -> Result<usize> {
+    let mut match_count = 0;
+    let prefix = filename.map(|s| format!("{s}:")).unwrap_or_default();
+    for line in reader.lines() {
+        let line = line?;
+        if let Some(matched) = match_regex(&line, pattern) {
+            match_count += 1;
+
+            let output = match line.find(&matched) {
+                Some(start) => {
+                    let end = start + matched.len();
+                    format!(
+                        "{}{}{}{}",
+                        prefix,
+                        &line[..start],
+                        &line[start..end].bright_red().bold(),
+                        &line[end..]
+                    )
+                }
+                None => format!("{prefix}{line}"),
+            };
+
+            println!("{output}");
+        }
+    }
+    Ok(match_count)
+}
+
+fn collect_files(inputs: &[PathBuf], recursive: bool) -> Result<Vec<PathBuf>> {
+    let mut files = Vec::new();
+    for input in inputs {
+        let meta = input.metadata()?;
+        if meta.is_file() {
+            files.push(input.clone());
+        } else if meta.is_dir() {
+            if !recursive {
+                bail!("{}: Is a directory", input.to_string_lossy());
+            }
+            files.extend(collect_dir(input)?);
+        }
+    }
+    Ok(files)
+}
+
+fn collect_dir(dir: &Path) -> Result<Vec<PathBuf>> {
+    let mut files = Vec::new();
+    for entry in dir.read_dir()? {
+        let entry = entry?;
+        let path = entry.path();
+        let meta = entry.metadata()?;
+        if meta.is_dir() {
+            files.extend(collect_dir(&path)?);
+        } else if meta.is_file() {
+            files.push(path);
+        }
+    }
+    Ok(files)
 }
